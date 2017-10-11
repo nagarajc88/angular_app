a19f527ddf7b874c18eeae7e841940c9b54a7dfb
nagarajc88 <nagarajc1988@gmail.com>
2017-10-11 18:25:17 +0530

    login completed successfully

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,10 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule }    from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-
-import {MdButtonModule, MdCheckboxModule} from '@angular/material';
-
-import { AppComponent } from './app.component';
+import { AppComponent }  from './app.component';
 import { routing }        from './app.routing';
-
 
 import { AlertComponent } from './_directives/index';
 import { AuthGuard } from './_guards/index';
@@ -17,34 +13,27 @@ import { HomeComponent } from './home/index';
 import { LoginComponent } from './login/index';
 import { RegisterComponent } from './register/index';
 
-
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    AlertComponent,
-    HomeComponent,
-    LoginComponent,
-    RegisterComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    routing,
-    MdButtonModule,
-    MdCheckboxModule
-  ],
-  exports: [
-  MdButtonModule,
-  MdCheckboxModule
-  ],
-  providers: [
-    AuthGuard,
-    AlertService,
-    AuthenticationService,
-    UserService
-  ],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        routing
+    ],
+    declarations: [
+        AppComponent,
+        AlertComponent,
+        HomeComponent,
+        LoginComponent,
+        RegisterComponent
+    ],
+    providers: [
+        AuthGuard,
+        AlertService,
+        AuthenticationService,
+        UserService
+    ],
+    bootstrap: [AppComponent]
 })
+
 export class AppModule { }
